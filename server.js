7e04bd22a53dd4b229523ae2b99189c074bfca44
CarlosStrider120 <sherifinho.augusto@gmail.com>
2022-10-24 21:01:34 -0300

    creating profile and image requests

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const PORT = 3000;
 
@@ -43,14 +43,43 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
     database.users.push({
-        id: database.users.length + 1,
+        id: `${database.users.length + 1}`,
         name: name,
         email: email,
         password: password,
         entries: 0,
         joined: new Date()
     })
-    res.json(database.users[database.users.length - 1])
+    res.json(database.users[database.users.length - 1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+});
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
 });
 
 app.listen(3000, () => {
